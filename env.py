24fd29ba468319bbf7861d1749836f9a45bca2e2
jax-parker <parkyjax711@gmail.com>
2023-07-25 08:48:37 +0000

    add cloudinary, env and settings

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ['DATABASE_URL'] = 'postgres://qnpzcjyq:Ucx31-bsa7wUPk1OVE2lYeL8PYBxqkmw@trumpet.db.elephantsql.com/qnpzcjyq'
 os.environ['SECRET_KEY'] = 'my^secret!key*'
+os.environ['CLOUDINARY'] = 'cloudinary://494875452291736:lfNiC1W9mrwpPnkw-elKxWW3Btk@dwvsz0fug'
