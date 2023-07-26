05b576a778248f09200bce953a8bec4aabad3335
jax-parker <parkyjax711@gmail.com>
2023-07-26 15:06:51 +0000

    book confirm delete page, urls, link on details page

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddBook, BooksView, BookDetail
+from .views import AddBook, BooksView, BookDetail, DeleteBook
 
 
 urlpatterns = [
     path("", AddBook.as_view(), name="add_book"),
     path("books/", BooksView.as_view(), name="books"),
     path("<slug:pk>/", BookDetail.as_view(), name="book_detail"),
+    path("delete/<slug:pk>/", DeleteBook.as_view(), name="delete_book"),
 ]
