128dfdd8d6268f0d6f59b647ef385e43e99d7e2f
jax-parker <parkyjax711@gmail.com>
2023-07-27 08:12:03 +0000

    add index page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import ListView
+from books.models import Books
 
 
 class Index(ListView):
     template_name = 'home/index.html'
+    model = Books
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
