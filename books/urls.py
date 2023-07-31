0b354a7bc35992a819a9cf8e457e50913ed58d8f
jax-parker <parkyjax711@gmail.com>
2023-07-31 13:37:06 +0000

    delete env.py file as per Student support

--- books/urls.py
+++ books/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 
 urlpatterns = [
+    path('user-books/', views.user_books, name='user_books'),
     path("add/", AddBook.as_view(), name="add_book"),
     path("confirm/<slug:pk>", DeleteBook.as_view(), name="confirm_delete"),
     path("delete/<slug:pk>/", ConfirmDelete.as_view(), name="delete_book"),
     path("edit/<slug:pk>/", EditReview.as_view(), name="edit_review"),
     path("<slug:pk>/", BookDetail.as_view(), name="book_detail"),
-    path('user-books/', views.user_books, name='user_books'),
     path("", BooksView.as_view(), name="books"),
 ]
--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ['DATABASE_URL'] = 'postgres://qnpzcjyq:Ucx31-bsa7wUPk1OVE2lYeL8PYBxqkmw@trumpet.db.elephantsql.com/qnpzcjyq'
-os.environ['SECRET_KEY'] = 'my^secret!key*'
-os.environ['CLOUDINARY_URL'] = 'cloudinary://494875452291736:lfNiC1W9mrwpPnkw-elKxWW3Btk@dwvsz0fug'
-os.environ['DEVELOPMENT'] = 'True'
