768e2ba2197c76e2addcad98cf3c06053845c186
jax-parker <parkyjax711@gmail.com>
2023-08-04 11:05:17 +0000

    add new heroku url

--- main/settings.py
+++ main/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['django-bookends.herokuapp.com',
                  '8000-jaxparker-bookends-r6l7rti2x81.ws-eu102.gitpod.io',
-                 'django-bookends-5c97f4949b50.herokuapp.com']
+                 'https://bookends-django-fe34319f080e.herokuapp.com']
 
 # Application definition
 
